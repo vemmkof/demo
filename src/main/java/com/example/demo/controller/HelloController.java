5433d6838b55bafc209ee6c88882d400d71bfba4
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-05-02 21:21:42 -0500

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/")
+	public Date date() {
+		return new Date();
+	}
+}
